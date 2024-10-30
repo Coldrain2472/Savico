7fdccc1b9c88ad6ff8dd7bf0da67b66e2e4e04ba
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-10-30 10:53:33 +0200

    Added data constants

---
+++ Savico.Infrastructure/Data/Constants/DataConstants.cs
@@ -0,0 +1,36 @@
+﻿namespace Savico.Infrastructure.Data.Constants
+{
+    public static class DataConstants
+    {
+        public const string DateTimeDefaultFormat = "dd/MM/yyyy";
+
+        public const string LengthErrorMessage = "{0} must be between {2} and {1} characters long!";
+
+        public const string RangeErrorMessage = "{0} must be a number between {1} and {2}!";
+
+        public static class UserConstants
+        {
+            // User constants
+        }
+
+        public static class BudgetConstants
+        {
+            // Budget constants
+        }
+
+        public static class ExpenseConstants
+        {
+            // Expense constants
+        }
+
+        public static class GoalConstants
+        {
+            // Goal constants
+        }
+
+        public static class IncomeConstants
+        {
+            // Income constants
+        }
+    }
+}
