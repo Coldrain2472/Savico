2be51e169eac52597e59f13e22c267413fe13eb2
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-01 16:31:21 +0200

    Added controllers

---
+++ Savico/Savico/Controllers/BudgetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Savico.Controllers
+{
+    public class BudgetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
