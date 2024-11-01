13a96a5d7979d966ced379e93c0ed353666b1f1b
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-01 21:29:50 +0200

    Tried to change the navbar (instead of showing the email, I wanted it to show the FirstName + LastName), but decided to revert the changes for now

--- Savico/Savico/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Savico/Savico/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Savico.Core.Models;
+using System.Security.Claims;
 
 namespace Savico.Areas.Identity.Pages.Account
 {
