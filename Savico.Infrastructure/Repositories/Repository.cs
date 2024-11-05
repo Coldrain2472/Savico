ec69c549362d1a6b8a324d72b463994ce5f97a26
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-05 09:25:49 +0200

    Fixed some usings

--- Savico.Infrastructure/Repositories/Repository.cs
+++ Savico.Infrastructure/Repositories/Repository.cs
@@ -4,6 +4,7 @@
     using Savico.Infrastructure.Repositories.Contracts;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Linq.Expressions;
     using System.Threading.Tasks;
 
