8f5491a1dd9975addc0dc3a1a0abb97023774ee0
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-05 09:25:06 +0200

    Added Income viewmodels

---
+++ Savico.Core/Models/ViewModels/Income/IncomeInputViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Savico.Core.Models.ViewModels.Income
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Savico.Infrastructure.Data.Constants.DataConstants.IncomeConstants;
+    using static Savico.Infrastructure.Data.Constants.DataConstants;
+    using System;
+
+    public class IncomeInputViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [StringLength(SourceMaxLength, MinimumLength = SourceMinLength, ErrorMessage = LengthErrorMessage)]
+        public string? Source { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Savico.Core/Models/ViewModels/Income/IncomeViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Savico.Core.Models.ViewModels.Income
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class IncomeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string? Source { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
