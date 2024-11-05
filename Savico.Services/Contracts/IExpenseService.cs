bda404b8b0a960d7044cba98aa6f876c22536f2f
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-05 17:40:48 +0200

    Updated the interface for ExpenseService

---
+++ Savico.Services/Contracts/IExpenseService.cs
@@ -0,0 +1,28 @@
+﻿namespace Savico.Services.Contracts
+{
+    using Savico.Core.Models.ViewModels.Category;
+    using Savico.Core.Models.ViewModels.Expense;
+
+    public interface IExpenseService
+    {
+        Task<IEnumerable<ExpenseViewModel>> GetAllExpensesAsync(string userId); // retrieves all expenses
+
+        Task AddExpenseAsync(ExpenseInputViewModel model, string userId); // adds an expense
+
+        Task DeleteExpenseAsync(int expenseId, string userId);  // deletes an expense
+
+        Task<ExpenseViewModel> GetExpenseByIdAsync(int expenseId, string userId); // retrieve an expense by a specific id
+
+        // Task UpdateExpenseAsync(int expenseId, ExpenseViewModel model, string userId); // updates an expense
+
+        Task EditExpenseAsync(int expenseId, ExpenseInputViewModel model, string userId); // edits an expense
+
+        Task<decimal?> CalculateRemainingBudgetAsync(string userId); // calculates the budget
+
+        Task<ExpenseInputViewModel> PrepareExpenseInputModelAsync(); // prepares the input model for edit/add
+
+        Task<string> GetCategoryNameByIdAsync(int categoryId); // retrieves a category name by id
+
+        Task<ICollection<CategoryViewModel>> GetCategories(); // retrieves the categories
+    }
+}
