ee930ab69654e61d440c3ee258871ee6261763b5
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-06 10:54:53 +0200

    Decided not to use repositories

--- Savico.Infrastructure/Common/PagedResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Savico.Infrastructure.Common
-{
-    public class PagedResult<T>
-    {
-        public List<T> Items { get; set; } = new List<T>();
-
-        public int TotalCount { get; set; }
-    }
-}
