c0c46fa54f8ae653831e6892a050553124149c25
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-07 16:18:23 +0200

    Added Goal ViewModels

---
+++ Savico.Core/Models/ViewModels/Goal/GoalInputViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Savico.Core.Models.ViewModels.Goal
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Savico.Infrastructure.Data.Constants.DataConstants;
+
+    public class GoalInputViewModel
+    {
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = RangeErrorMessage)]
+        public decimal TargetAmount { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = RangeErrorMessage)]
+        public decimal CurrentAmount { get; set; }
+
+        [Required]
+        public DateTime TargetDate { get; set; }
+    }
+}
---
+++ Savico.Core/Models/ViewModels/Goal/GoalViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Savico.Core.Models.ViewModels.Goal
+{
+    public class GoalViewModel
+    {
+        public int Id { get; set; }
+
+        public decimal TargetAmount { get; set; }
+
+        public decimal CurrentAmount { get; set; }
+
+        public DateTime TargetDate { get; set; }
+    }
+}
