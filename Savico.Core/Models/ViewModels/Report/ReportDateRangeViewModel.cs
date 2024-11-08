dc60319384eb60a50b35abffbba70aaea1ea94fd
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-08 10:14:47 +0200

    Added new viewModels

---
+++ Savico.Core/Models/ViewModels/Report/ReportDateRangeViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Savico.Core.Models.ViewModels.Report
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ReportDateRangeViewModel
+    {
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Savico.Core/Models/ViewModels/Report/ReportViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Savico.Core.Models.ViewModels.Report
+{
+    public class ReportViewModel
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+
+        public List<IncomeViewModel> Incomes { get; set; } = new List<IncomeViewModel>(); // add such collection in the entity?
+        public List<ExpenseViewModel> Expenses { get; set; } = new List<ExpenseViewModel>(); // add such collection in the entity?
+    }
+
+    // Supporting ViewModels for displaying incomes and expenses
+    public class IncomeViewModel
+    {
+        public DateTime Date { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string? Source { get; set; }
+    }
+
+    public class ExpenseViewModel
+    {
+        public DateTime Date { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string? Description { get; set; }
+    }
+
+}
