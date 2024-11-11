4786d64eb6f4a8e05b951572d561dc73e2d08012
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-11 16:47:27 +0200

    Added new Entity to the db

---
+++ Savico.Infrastructure/Data/Models/Tip.cs
@@ -0,0 +1,14 @@
+﻿namespace Savico.Infrastructure.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Savico.Infrastructure.Data.Constants.DataConstants.TipConstants;
+
+    public class Tip
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string? Content { get; set; }
+    }
+}
