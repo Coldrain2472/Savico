f828614583e05085594cf54a9a00e68922c3f753
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-12 16:19:39 +0200

    Created the Report's ViewModels

---
+++ Savico.Core/Models/ViewModels/Report/ReportInputViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Savico.Core.Models.ViewModels.Report
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ReportInputViewModel
+    {
+        [Required]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+    }
+}
--- Savico.Core/Models/ViewModels/Report/ReportViewModel.cs
+++ Savico.Core/Models/ViewModels/Report/ReportViewModel.cs
@@ -2,32 +2,18 @@
 {
     public class ReportViewModel
     {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = null!;
+
         public DateTime StartDate { get; set; }
+
         public DateTime EndDate { get; set; }
+
         public decimal TotalIncome { get; set; }
+
         public decimal TotalExpense { get; set; }
 
-        public List<IncomeViewModel> Incomes { get; set; } = new List<IncomeViewModel>(); // add such collection in the entity?
-        public List<ExpenseViewModel> Expenses { get; set; } = new List<ExpenseViewModel>(); // add such collection in the entity?
+        public string? Currency { get; set; }
     }
-
-    // Supporting ViewModels for displaying incomes and expenses
-    public class IncomeViewModel
-    {
-        public DateTime Date { get; set; }
-
-        public decimal Amount { get; set; }
-
-        public string? Source { get; set; }
-    }
-
-    public class ExpenseViewModel
-    {
-        public DateTime Date { get; set; }
-
-        public decimal Amount { get; set; }
-
-        public string? Description { get; set; }
-    }
-
 }
