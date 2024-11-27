dd854aceb2b82ba72df5c7b22dde8ba4f418da76
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-11-27 10:47:53 +0200

    Create AccountController.cs

---
+++ Savico/Savico/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿namespace Savico.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Savico.Core.Models;
+
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(SignInManager<User> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+    }
+}
