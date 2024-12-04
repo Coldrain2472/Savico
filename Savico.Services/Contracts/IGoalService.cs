4430ee53a36ba428a59695e78c8e9092d2fd3a6d
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-12-04 10:04:05 +0200

    Added comments

--- Savico.Services/Contracts/IGoalService.cs
+++ Savico.Services/Contracts/IGoalService.cs
@@ -4,22 +4,22 @@
 
     public interface IGoalService
     {
-        Task<GoalInputViewModel> GetGoalForEditAsync(int goalId, string userId);
+        Task<GoalInputViewModel> GetGoalForEditAsync(int goalId, string userId); // retrieves a goal for edit
 
-        Task<IEnumerable<GoalViewModel>> GetAllGoalsAsync(string userId);
+        Task<IEnumerable<GoalViewModel>> GetAllGoalsAsync(string userId); // retrieves all goals
 
-        Task<GoalContributeViewModel> GetGoalContributeViewModelAsync(int goalId, string userId);
+        Task<GoalContributeViewModel> GetGoalContributeViewModelAsync(int goalId, string userId); // retrieves the goal contribute view model
 
-        Task ContributeToGoalAsync(GoalContributeViewModel model, string userId);
+        Task ContributeToGoalAsync(GoalContributeViewModel model, string userId); // contributes to a goal
 
-        Task<GoalInputViewModel> GetGoalInputViewModelAsync(string userId);
+        Task<GoalInputViewModel> GetGoalInputViewModelAsync(string userId); // retrieves the goal input view model
 
-        Task AddGoalAsync(GoalInputViewModel model, string userId);
+        Task AddGoalAsync(GoalInputViewModel model, string userId); // adds a goal
 
-        Task<GoalViewModel> GetGoalByIdAsync(int id, string userId);
+        Task<GoalViewModel> GetGoalByIdAsync(int id, string userId); // retrieves a goal by id
 
-        Task UpdateGoalAsync(int id, GoalInputViewModel model, string userId);
+        Task UpdateGoalAsync(int id, GoalInputViewModel model, string userId); // updates a goal
 
-        Task DeleteGoalAsync(int id, string userId);
+        Task DeleteGoalAsync(int id, string userId); // deletes a goal
     }
 }
